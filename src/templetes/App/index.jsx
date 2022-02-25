03601f336bfadd90f276e2d28611224c67782af7
Vinicius Ferreira <viniciuferreira32@gmail.com>
2022-02-24 21:56:08 -0300

    Criando meu segundo hook

--- src/templetes/App/index.jsx
+++ src/templetes/App/index.jsx
@@ -1,18 +1,36 @@
-import React from 'react';
-import Posts from '../../components/Posts';
+// import React from 'react';
+// import Posts from '../../components/Posts';
 
-import { CounterProvider } from '../../contexts/CounterProvider';
-import { PostsProvider } from '../../contexts/PostsProvider';
+// import { CounterProvider } from '../../contexts/CounterProvider';
+// import { PostsProvider } from '../../contexts/PostsProvider';
+
+// function App() {
+//   return (
+//     <CounterProvider>
+//       <PostsProvider>
+//         <h1>Posts</h1>
+//         <Posts />
+//       </PostsProvider>
+//     </CounterProvider>
+//   );
+// }
+
+// export default App;
+
+/*eslint-disable*/
+const useFetch = (url, options) => {
+  console.log('hook')
+  return [123, 'load'];
+};
+
+import React from 'react';
 
 function App() {
-  return (
-    <CounterProvider>
-      <PostsProvider>
-        <h1>Posts</h1>
-        <Posts />
-      </PostsProvider>
-    </CounterProvider>
-  );
+
+  const [ol, load] = useFetch()
+
+  console.log(ol, load)
+  return <div>App</div>;
 }
 
 export default App;

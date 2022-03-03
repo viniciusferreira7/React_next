257af7c6d2348f589a142f4547215cb9fb763cd2
Vinicius Ferreira <viniciuferreira32@gmail.com>
2022-03-02 21:56:41 -0300

    Finalizado o custom hook useAsync

--- src/templetes/App/index.jsx
+++ src/templetes/App/index.jsx
@@ -124,45 +124,68 @@
 // };
 
 import React, { useCallback, useEffect, useState } from 'react';
+//eslint-disable-next-line
+export const useAsync = (asyncFunction, shouldRun) => {
+  const [state, setState] = useState({
+    result: null,
+    error: null,
+    status: 'idle',
+  });
 
-/*eslint-disable */
+  const run = useCallback(async () => {
+    await new Promise((r) => setTimeout(r, 2000));
 
-const useAsync = (asyncFunction) => {
-  const [result, setResult] = useState(null)
-  const [error, setError] = useState(null)
-  const [status, setStatus] = useState('idle')
-
-  const run = useCallback(() => {
-    setResult(null)
-    setError(null)
-    setStatus('pending')
+    setState({
+      result: null,
+      error: null,
+      status: 'pending',
+    });
 
     return asyncFunction()
-      .then(response => {
-        setResult(response)
+      .then((response) => {
+        setState({
+          result: response,
+          error: null,
+          status: 'settled',
+        });
       })
-      .catch(error => {
-        setError(error)
-      })
-  }, [asyncFunction])
-
-}
-
-export const App = () => {
-  const [posts, setPosts] = useState(null)
+      .catch((error) => {
+        setState({
+          result: null,
+          error: error,
+          status: 'settled',
+        });
+      });
+  }, [asyncFunction]);
 
   useEffect(() => {
-    const fetchData = async () => {
-      const data = await fetch('https://jsonplaceholder.typicode.com/posts')
-      const json = await data.json()
-      setPosts(json)
-      return json
+    if (shouldRun) {
+      run();
     }
-    fetchData()
-  }, [])
+  }, [shouldRun, run]);
 
+  return [run, state];
+};
 
-  return (
-    <pre>{JSON.stringify(posts, null, 2)}</pre>
-  )
-}
+export const fetchData = async () => {
+  await new Promise((r) => setTimeout(r, 2000));
+
+  const data = await fetch('https://jsonplaceholder.typicode.com/posts');
+  const json = await data.json();
+
+  return json;
+};
+
+export const App = () => {
+  //eslint-disable-next-line
+  const [reFetchData, state] = useAsync(fetchData, true);
+
+  // useEffect(() => {
+  //   reFetchData();
+  // }, [reFetchData]);
+
+  if (state.status === 'idle') return <h2>Nada executando</h2>;
+  if (state.status === 'pending') return <h2>Loading...</h2>;
+  if (state.status === 'error') return <pre>{JSON.stringify(state.error, null, 2)}</pre>;
+  if (state.status === 'settled') return <pre>{JSON.stringify(state.result, null, 2)}</pre>;
+};

e2c131d88283889fd799b18f6c5ea6a007869f52
Vinicius Ferreira <viniciuferreira32@gmail.com>
2022-03-03 22:23:17 -0300

    aprendendo a usaar o useLayoutEffect

--- src/templetes/App/index.jsx
+++ src/templetes/App/index.jsx
@@ -1,191 +1,159 @@
-// // import React from 'react';
-// // import Posts from '../../components/Posts';
+// // // import React from 'react';
+// // // import Posts from '../../components/Posts';
 
-// // import { CounterProvider } from '../../contexts/CounterProvider';
-// // import { PostsProvider } from '../../contexts/PostsProvider';
+// // // import { CounterProvider } from '../../contexts/CounterProvider';
+// // // import { PostsProvider } from '../../contexts/PostsProvider';
 
-// // function App() {
-// //   return (
-// //     <CounterProvider>
-// //       <PostsProvider>
-// //         <h1>Posts</h1>
-// //         <Posts />
-// //       </PostsProvider>
-// //     </CounterProvider>
-// //   );
-// // }
+// // // function App() {
+// // //   return (
+// // //     <CounterProvider>
+// // //       <PostsProvider>
+// // //         <h1>Posts</h1>
+// // //         <Posts />
+// // //       </PostsProvider>
+// // //     </CounterProvider>
+// // //   );
+// // // }
 
-// // export default App;
+// // // export default App;
 
-// import React, { useEffect, useRef, useState } from 'react';
+// // import React, { useEffect, useRef, useState } from 'react';
 
-// const isObjectEqual = (objA, objB) => {
-//   return JSON.stringify(objA) === JSON.stringify(objB);
-// };
+// // const isObjectEqual = (objA, objB) => {
+// //   return JSON.stringify(objA) === JSON.stringify(objB);
+// // };
 
-// export const useFetch = (url, options) => {
-//   const [result, setResult] = useState(null);
-//   const [loading, setLoading] = useState(false);
-//   const [shouldLoading, setShouldLoading] = useState(false);
-//   const urlRef = useRef(url);
-//   const optionsRef = useRef(options);
+// // export const useFetch = (url, options) => {
+// //   const [result, setResult] = useState(null);
+// //   const [loading, setLoading] = useState(false);
+// //   const [shouldLoading, setShouldLoading] = useState(false);
+// //   const urlRef = useRef(url);
+// //   const optionsRef = useRef(options);
 
-//   useEffect(() => {
-//     let change = false;
+// //   useEffect(() => {
+// //     let change = false;
 
-//     if (!isObjectEqual(url, urlRef.current)) {
-//       urlRef.current = url;
-//       change = true;
-//     }
-//     if (!isObjectEqual(options, optionsRef.current)) {
-//       optionsRef.current = options;
-//       change = true;
-//     }
+// //     if (!isObjectEqual(url, urlRef.current)) {
+// //       urlRef.current = url;
+// //       change = true;
+// //     }
+// //     if (!isObjectEqual(options, optionsRef.current)) {
+// //       optionsRef.current = options;
+// //       change = true;
+// //     }
 
-//     if (change) setShouldLoading((s) => !s);
-//   }, [url, options]);
+// //     if (change) setShouldLoading((s) => !s);
+// //   }, [url, options]);
 
-//   useEffect(() => {
-//     const controller = new AbortController();
-//     const signal = controller.signal;
-//     let wait = false;
+// //   useEffect(() => {
+// //     const controller = new AbortController();
+// //     const signal = controller.signal;
+// //     let wait = false;
 
-//     setLoading(true);
+// //     setLoading(true);
 
-//     const fetchData = async () => {
-//       await new Promise((r) => {
-//         setTimeout(r, 1000);
-//       });
-//       try {
-//         const response = await fetch(urlRef.current, { signal, ...optionsRef.current });
-//         const jsonResult = await response.json();
+// //     const fetchData = async () => {
+// //       await new Promise((r) => {
+// //         setTimeout(r, 1000);
+// //       });
+// //       try {
+// //         const response = await fetch(urlRef.current, { signal, ...optionsRef.current });
+// //         const jsonResult = await response.json();
 
-//         if (!wait) {
-//           setResult(jsonResult);
-//           setLoading(false);
-//         }
-//       } catch (e) {
-//         if (!wait) {
-//           setLoading(false);
-//         }
-//         throw e;
-//       }
-//     };
-//     fetchData();
+// //         if (!wait) {
+// //           setResult(jsonResult);
+// //           setLoading(false);
+// //         }
+// //       } catch (e) {
+// //         if (!wait) {
+// //           setLoading(false);
+// //         }
+// //         throw e;
+// //       }
+// //     };
+// //     fetchData();
 
-//     return () => {
-//       wait = true;
-//       controller.abort();
-//     };
-//   }, [shouldLoading]);
+// //     return () => {
+// //       wait = true;
+// //       controller.abort();
+// //     };
+// //   }, [shouldLoading]);
 
-//   return [result, loading];
-// };
+// //   return [result, loading];
+// // };
+
+// // export const App = () => {
+// //   const [postsId, setPostsId] = useState('');
+// //   const [result, loading] = useFetch(`https://jsonplaceholder.typicode.com/posts/${postsId}`, {
+// //     headers: {
+// //       abc: '1',
+// //     },
+// //   });
+
+// //   useEffect(() => {
+// //     console.log('mudou o id do:', postsId);
+// //   }, [postsId]);
+
+// //   const handleClick = (id) => {
+// //     setPostsId(id);
+// //   };
+
+// //   if (loading) {
+// //     return <p>Loading...</p>;
+// //   }
+
+// //   if (!loading && result) {
+// //     return (
+// //       <div>
+// //         {result?.length > 0 ? (
+// //           result.map((p) => (
+// //             <div key={`posts-${p.id}`} onClick={() => handleClick(p.id)}>
+// //               <p>{p.title}</p>
+// //             </div>
+// //           ))
+// //         ) : (
+// //           <div key={`posts-${result.id}`} onClick={() => handleClick('')}>
+// //             <p>{result.title}</p>
+// //           </div>
+// //         )}
+// //       </div>
+// //     );
+// //   }
+
+// //   return <h1>Oi</h1>;
+// // };
 
 // export const App = () => {
-//   const [postsId, setPostsId] = useState('');
-//   const [result, loading] = useFetch(`https://jsonplaceholder.typicode.com/posts/${postsId}`, {
-//     headers: {
-//       abc: '1',
-//     },
-//   });
+//   //eslint-disable-next-line
+//   const [reFetchData, state] = useAsync(fetchData, true);
 
-//   useEffect(() => {
-//     console.log('mudou o id do:', postsId);
-//   }, [postsId]);
+//   // useEffect(() => {
+//   //   reFetchData();
+//   // }, [reFetchData]);
 
-//   const handleClick = (id) => {
-//     setPostsId(id);
-//   };
-
-//   if (loading) {
-//     return <p>Loading...</p>;
-//   }
-
-//   if (!loading && result) {
-//     return (
-//       <div>
-//         {result?.length > 0 ? (
-//           result.map((p) => (
-//             <div key={`posts-${p.id}`} onClick={() => handleClick(p.id)}>
-//               <p>{p.title}</p>
-//             </div>
-//           ))
-//         ) : (
-//           <div key={`posts-${result.id}`} onClick={() => handleClick('')}>
-//             <p>{result.title}</p>
-//           </div>
-//         )}
-//       </div>
-//     );
-//   }
-
-//   return <h1>Oi</h1>;
+//   if (state.status === 'idle') return <h2>Nada executando</h2>;
+//   if (state.status === 'pending') return <h2>Loading...</h2>;
+//   if (state.status === 'error') return <pre>{JSON.stringify(state.error, null, 2)}</pre>;
+//   if (state.status === 'settled') return <pre>{JSON.stringify(state.result, null, 2)}</pre>;
 // };
 
-import React, { useCallback, useEffect, useState } from 'react';
-//eslint-disable-next-line
-export const useAsync = (asyncFunction, shouldRun) => {
-  const [state, setState] = useState({
-    result: null,
-    error: null,
-    status: 'idle',
-  });
-
-  const run = useCallback(async () => {
-    await new Promise((r) => setTimeout(r, 2000));
-
-    setState({
-      result: null,
-      error: null,
-      status: 'pending',
-    });
-
-    return asyncFunction()
-      .then((response) => {
-        setState({
-          result: response,
-          error: null,
-          status: 'settled',
-        });
-      })
-      .catch((error) => {
-        setState({
-          result: null,
-          error: error,
-          status: 'settled',
-        });
-      });
-  }, [asyncFunction]);
-
-  useEffect(() => {
-    if (shouldRun) {
-      run();
-    }
-  }, [shouldRun, run]);
-
-  return [run, state];
-};
-
-export const fetchData = async () => {
-  await new Promise((r) => setTimeout(r, 2000));
-
-  const data = await fetch('https://jsonplaceholder.typicode.com/posts');
-  const json = await data.json();
-
-  return json;
-};
+import React, { useState } from 'react';
 
 export const App = () => {
-  //eslint-disable-next-line
-  const [reFetchData, state] = useAsync(fetchData, true);
+  const [counted, setCounted] = useState([0, 1, 2, 3, 4]);
 
-  // useEffect(() => {
-  //   reFetchData();
-  // }, [reFetchData]);
+  const handleClick = () => {
+    setCounted((c) => [...c, +c.splice(-1) + 1]);
+  };
 
-  if (state.status === 'idle') return <h2>Nada executando</h2>;
-  if (state.status === 'pending') return <h2>Loading...</h2>;
-  if (state.status === 'error') return <pre>{JSON.stringify(state.error, null, 2)}</pre>;
-  if (state.status === 'settled') return <pre>{JSON.stringify(state.result, null, 2)}</pre>;
+  console.log(counted.slice(-1));
+
+  return (
+    <>
+      <button onClick={() => handleClick()}>Counter: {counted.slice(-1)}</button>
+      {counted.map((c) => (
+        <p key={`c-${c}`}>{c}</p>
+      ))}
+    </>
+  );
 };

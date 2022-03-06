e8722308cf863d1a9fb1da719ca8f66c0321330b
Vinicius Ferreira <viniciuferreira32@gmail.com>
2022-03-05 22:30:54 -0300

    Inicio do useLayoutEffect

--- src/templetes/App/index.jsx
+++ src/templetes/App/index.jsx
@@ -138,22 +138,29 @@
 // };
 
 import React, { useState } from 'react';
+import { useLayoutEffect, useRef } from 'react/cjs/react.development';
 
 export const App = () => {
-  const [counted, setCounted] = useState([0, 1, 2, 3, 4]);
+  const [counted, setCounted] = useState([1, 2, 3, 4]);
+  const divRef = useRef();
+
+  useLayoutEffect(() => {
+    const n = Date.now();
+    while (Date.now() < n + 2000) divRef.current.scrollTop = divRef.current.scrollHeight;
+  });
 
   const handleClick = () => {
-    setCounted((c) => [...c, +c.splice(-1) + 1]);
+    setCounted((c) => [...c, +c.slice(-1) + 1]);
   };
-
-  console.log(counted.slice(-1));
 
   return (
     <>
-      <button onClick={() => handleClick()}>Counter: {counted.slice(-1)}</button>
-      {counted.map((c) => (
-        <p key={`c-${c}`}>{c}</p>
-      ))}
+      <button onClick={() => handleClick()}>Count + {counted.slice(-1)}</button>
+      <div ref={divRef} style={{ height: '100px', width: '100px', overflowY: 'scroll' }}>
+        {counted.map((c) => (
+          <p key={`c-${c}`}>{c}</p>
+        ))}
+      </div>
     </>
   );
 };
